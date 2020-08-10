13322e47b8775dd5f58f5e2c780c41aea8e0bc1e
zar686 <azamzariff@gmail.com>
2020-08-10 18:32:56 +0100

    updates

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get('/', function(req, res) {
             <span class="item-text">${item.text}</span>
             <div>
               <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
+              <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
             </div>
           </li>`
         }).join('')}
@@ -77,7 +77,11 @@ app.post('/update-item', function(req, res) {
   })
   })
 
-
+  app.post('/delete-item', function(req, res) {
+    db.collection('items').deleteOne({_id: new mongodb.ObjectId(req.body.id)}, function() {
+      res.send('success')
+    })
+    })
 
 // needed for testing
 module.exports = app
